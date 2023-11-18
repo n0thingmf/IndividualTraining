aa4bed36cd02528db9532f111604b23f6e964817
Mijail <Mijail@MacBook-Pro.local>
2023-11-18 15:01:39 -0500

    FACENEMY

--- week2/FACENEMY - Face the mate.cpp
+++ week2/FACENEMY - Face the mate.cpp
@@ -8,13 +8,19 @@
 #define bpc __builtin_popcountll//numero de bits para long long
 #define bclz __builtin_clzll//leading zeros para ll
 #define max_bit(A) 31-__builtin_clz(A)
-#define point complex<int>
+#define point complex<long long>
 #define x real()
 #define y imag()
 using namespace std;
 
-int cross(point a,point b){
+long long cross(point a,point b){
     return imag(conj(a)*b);
+}
+long long dot(point a,point b){
+    return real(conj(a)*b);
+}
+long long n2(point a){
+    return a.x*a.x+a.y*a.y;
 }
 int main(){
     ios::sync_with_stdio(false);
@@ -29,6 +35,29 @@ int main(){
             cin>>u>>v;
             p[i]=point(u,v);
         }
+        /*ll cs1=cross(p[2]-p[0],p[1]-p[0]),cs2=cross(p[0]-p[2],p[3]-p[2]);
+        if(cs1*cs2>=0){
+            ll cs=cs2*cross(p[1]-p[0],p[2]-p[3]);
+            if(cs>0) cout<<"Hero\n";
+            else if(cs<0) cout<<"Heroine\n";
+            else cout<<"0\n";
+        }else{
+            cout<<"other\n";
+        }*/
+        ll d1=dot(p[2]-p[0],p[1]-p[0]),d2=dot(p[0]-p[2],p[3]-p[2]);
+        if(d1*d2>=0){
+            ll c1,c2;
+            if(d1>0 || d2>0) c1=d1*d1*n2(p[3]-p[2]),c2=d2*d2*n2(p[1]-p[0]);
+            else c1=d2*d2*n2(p[1]-p[0]),c2=d1*d1*n2(p[3]-p[2]);
+            if(c1==c2) cout<<"0\n";
+            else if(c1>c2) cout<<"Hero\n";
+            else cout<<"Heroine\n";
+    
+        }else{
+            if(d1>d2) cout<<"Hero\n";
+            else cout<<"Heroine\n";
+        }
+
     }
     return 0;
     // you should actually read the stuff at the bottom
